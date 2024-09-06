ad9cfb24b1f241093df6943f6239ffc262ef453a
Anthony Salvione <anthony98177@gmail.com>
2024-09-05 19:06:19 -0700

    Worked out some editor settings and singleton instantiation for the NetworkManager

--- BrodentServer/Assets/Scripts/Networking/NetworkManager.cs
+++ BrodentServer/Assets/Scripts/Networking/NetworkManager.cs
@@ -1,18 +1,24 @@
+// <copyright file="NetworkManager.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
 using System.Collections;
 using System.Collections.Generic;
+using Riptide;
 using UnityEngine;
 
+/// <summary>
+/// Public definition of the NetworkManager class for the server.
+/// </summary>
 public class NetworkManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
     }
 }
--- Brodents/Assets/Scripts/Networking/NetworkManager.cs
+++ Brodents/Assets/Scripts/Networking/NetworkManager.cs
@@ -1,18 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
+// <copyright file="NetworkManager.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using Riptide;
+using Riptide.Utils;
 using UnityEngine;
 
+/// <summary>
+/// Enumerator Id's of Server to Slient requests.
+/// </summary>
+public enum ServerToClientId : ushort
+{
+    /// <summary> Ensures client is on same tick as server. </summary>
+    Sync = 1,
+}
+
+/// <summary>
+/// Enumerator Id's of Client to Server requests.
+/// </summary>
+public enum ClientToServerId : ushort
+{
+    /// <summary> Sends initial login request. </summary>
+    Login = 1,
+}
+
+/// <summary>
+/// Instantiates the NetworkManager class.
+/// </summary>
 public class NetworkManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private static NetworkManager singleton;
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Gets the private singleton and set's the public static singleton to match.
+    /// </summary>
+    public static NetworkManager Singleton
     {
-        
+        get => singleton;
+        private set
+        {
+            if (singleton == null)
+            {
+                singleton = value;
+            }
+            else if (singleton != value)
+            {
+                Debug.Log($"{nameof(NetworkManager)} instance already exists, destroyed duplicate");
+                Destroy(value);
+            }
+        }
     }
 }
